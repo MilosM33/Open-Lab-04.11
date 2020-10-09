d97d6933117a358953041854cbe8bb750f689074
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 16:23:03 +0200

    init

--- Open-Lab-04.11/StringTools.cs
+++ Open-Lab-04.11/StringTools.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_04._11
 {
     public class StringTools
     {
-        public string AlphabetSoup(string str)
-        {
-            throw new NotImplementedException();
-        }
+        public string AlphabetSoup(string str) => string.Join("", str.OrderBy(p => p));
+        
     }
 }
